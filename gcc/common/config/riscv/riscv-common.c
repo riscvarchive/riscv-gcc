e2bd2c1fe418446a91558eb924196f14aafbf2f7
Andrew Waterman <waterman@cs.berkeley.edu>
2016-10-24 15:31:11 -0700

    Set -free for -O2 and above.

--- gcc/common/config/riscv/riscv-common.c
+++ gcc/common/config/riscv/riscv-common.c
@@ -126,6 +126,7 @@ static const struct default_options riscv_option_optimization_table[] =
   {
     { OPT_LEVELS_1_PLUS, OPT_fsection_anchors, NULL, 1 },
     { OPT_LEVELS_1_PLUS, OPT_fomit_frame_pointer, NULL, 1 },
+    { OPT_LEVELS_2_PLUS, OPT_free, NULL, 1 },
     { OPT_LEVELS_NONE, 0, NULL, 0 }
   };
 
