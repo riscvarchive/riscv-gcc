gcc-mirror/gcc

commit 1edfd7ddf58c3b8122b46d532b512d3d5d31ab63

2018-09-17  Paul Thomas  <pault@gcc.gnu.org>
	PR fortran/85954
	* resolve.c (resolve_assoc_var): If the target expression is a
	deferred charlen dummy and the associate name shares the
	charlen, generate a new one. Make sure that new charlens are in
	the namespace list so that they get cleaned up.
	* trans-array.c (gfc_is_reallocatable_lhs): Associate names are
	not reallocatable.
	* trans-decl.c (gfc_get_symbol_decl): Put deferred character
	length dummy and result arrays on the deferred initialization
	list so that the variable length arrays can be correctly dealt
	with.
	* trans-expr.c (gfc_conv_string_length): Return if 'expr' is
	NULL rather than ICEing..

2018-09-17  Paul Thomas  <pault@gcc.gnu.org>

	PR fortran/85954
	* gfortran.dg/deferred_character_21.f90 : New test.



git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@264358 138bc75d-0d04-0410-961f-82ee72b054a4

gcc/fortran/trans-array.c
@@ -9520,6 +9520,9 @@ gfc_is_reallocatable_lhs (gfc_expr *expr)
 
   sym = expr->symtree->n.sym;
 
+  if (sym->attr.associate_var)
+    return false;
+
   /* An allocatable class variable with no reference.  */
   if (sym->ts.type == BT_CLASS
       && CLASS_DATA (sym)->attr.allocatable

gcc/fortran/resolve.c
@@ -8744,6 +8744,14 @@ resolve_assoc_var (gfc_symbol* sym, bool resolve_target)
       if (!sym->ts.u.cl)
 	sym->ts.u.cl = target->ts.u.cl;
 
+      if (sym->ts.deferred && target->expr_type == EXPR_VARIABLE
+	  && target->symtree->n.sym->attr.dummy
+	  && sym->ts.u.cl == target->ts.u.cl)
+	{
+	  sym->ts.u.cl = gfc_new_charlen (sym->ns, NULL);
+	  sym->ts.deferred = 1;
+	}
+
       if (!sym->ts.u.cl->length
 	  && !sym->ts.deferred
 	  && target->expr_type == EXPR_CONSTANT)
@@ -8756,7 +8764,7 @@ resolve_assoc_var (gfc_symbol* sym, bool resolve_target)
 		|| sym->ts.u.cl->length->expr_type != EXPR_CONSTANT)
 		&& target->expr_type != EXPR_VARIABLE)
 	{
-	  sym->ts.u.cl = gfc_get_charlen();
+	  sym->ts.u.cl = gfc_new_charlen (sym->ns, NULL);
 	  sym->ts.deferred = 1;
 
 	  /* This is reset in trans-stmt.c after the assignment

gcc/fortran/trans-decl.c
@@ -1510,6 +1510,13 @@ gfc_get_symbol_decl (gfc_symbol * sym)
       /* Dummy variables should already have been created.  */
       gcc_assert (sym->backend_decl);
 
+      /* However, the string length of deferred arrays must be set.  */
+      if (sym->ts.type == BT_CHARACTER
+	  && sym->ts.deferred
+	  && sym->attr.dimension
+	  && sym->attr.allocatable)
+	gfc_defer_symbol_init (sym);
+
       if (sym->attr.pointer && sym->attr.dimension && sym->ts.type != BT_CLASS)
 	GFC_DECL_PTR_ARRAY_P (sym->backend_decl) = 1;
 

gcc/fortran/trans-expr.c
@@ -2237,7 +2237,8 @@ gfc_conv_string_length (gfc_charlen * cl, gfc_expr * expr, stmtblock_t * pblock)
   if (!cl->length)
     {
       gfc_expr* expr_flat;
-      gcc_assert (expr);
+      if (!expr)
+	return;
       expr_flat = gfc_copy_expr (expr);
       flatten_array_ctors_without_strlen (expr_flat);
       gfc_resolve_expr (expr_flat);

gcc/testsuite/gfortran.dg/deferred_character_21.f90
@@ -1,0 +1,29 @@
+! { dg-do compile }
+! { dg-options "-O3" }
+!
+! Tests the fix for PR85954 in which the gimplifier could not determine
+! the space required for the dummy argument data types, when inlining the
+! subroutines.
+!
+! Contributed by G.Steinmetz  <gscfq@t-online.de>
+!
+program p
+   character(kind=1,len=:), allocatable :: z(:)
+   allocate (z, source = ["xyz"])
+   print *, allocated(z), size(z), len(z), z
+   call s(z)
+   call t(z)
+contains
+   subroutine s(x)
+      character(kind=1,len=:), allocatable :: x(:)
+      x = ['abcd']
+      print *, allocated(x), size(x), len(x), x
+   end
+   subroutine t(x)
+      character(kind=1,len=:), allocatable :: x(:)
+      associate (y => x)
+         y = ['abc']
+      end associate
+      print *, allocated(x), size(x), len(x), x
+   end
+end

riscvarchive/riscv-gcc

commit 941c0a0ae8ce74982f4e4e06bfe175f3b25755df

2018-12-21  Paul Thomas  <pault@gcc.gnu.org>
	PR fortran/87359
	* trans-array.c (gfc_is_reallocatable_lhs): Correct the problem
	introduced by r264358, which prevented components of associate
	names from being reallocated on assignment.


2018-12-21  Paul Thomas  <pault@gcc.gnu.org>

	PR fortran/87359
	* gfortran.dg/associate_40.f90 : New test.



git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-8-branch@267342 138bc75d-0d04-0410-961f-82ee72b054a4

gcc/fortran/trans-array.c
@@ -9497,11 +9497,12 @@ gfc_is_reallocatable_lhs (gfc_expr *expr)
 
   sym = expr->symtree->n.sym;
 
-  if (sym->attr.associate_var)
+  if (sym->attr.associate_var && !expr->ref)
     return false;
 
   /* An allocatable class variable with no reference.  */
   if (sym->ts.type == BT_CLASS
+      && !sym->attr.associate_var
       && CLASS_DATA (sym)->attr.allocatable
       && expr->ref && expr->ref->type == REF_COMPONENT
       && strcmp (expr->ref->u.c.component->name, "_data") == 0
@@ -9510,9 +9511,10 @@ gfc_is_reallocatable_lhs (gfc_expr *expr)
 
   /* An allocatable variable.  */
   if (sym->attr.allocatable
-	&& expr->ref
-	&& expr->ref->type == REF_ARRAY
-	&& expr->ref->u.ar.type == AR_FULL)
+      && !sym->attr.associate_var
+      && expr->ref
+      && expr->ref->type == REF_ARRAY
+      && expr->ref->u.ar.type == AR_FULL)
     return true;
 
   /* All that can be left are allocatable components.  */

gcc/testsuite/gfortran.dg/associate_40.f90
@@ -1,0 +1,96 @@
+! { dg-do compile }
+! { dg-options "-fdump-tree-original" }
+!
+! Test the fix for the second part of PR87359 in which the reallocation on
+! assignment for components of associate names was disallowed by r264358.
+! -fcheck-all exposed the mismatch in array shapes. The deallocations at
+! the end of the main program are there to make sure that valgrind does
+! not report an memory leaks.
+!
+! Contributed by Juergen Reuter  <juergen.reuter@desy.de>
+!
+module phs_fks
+  implicit none
+  private
+  public :: phs_identifier_t
+  public :: phs_fks_t
+  type :: phs_identifier_t
+     integer, dimension(:), allocatable :: contributors
+  contains
+    procedure :: init => phs_identifier_init
+  end type phs_identifier_t
+
+  type :: phs_fks_t
+     type(phs_identifier_t), dimension(:), allocatable :: phs_identifiers
+  end type phs_fks_t
+contains
+
+  subroutine phs_identifier_init &
+     (phs_id, contributors)
+     class(phs_identifier_t), intent(out) :: phs_id
+     integer, intent(in), dimension(:) :: contributors
+     allocate (phs_id%contributors (size (contributors)))
+     phs_id%contributors = contributors
+   end subroutine phs_identifier_init
+
+end module phs_fks
+
+!!!!!
+
+module instances
+  use phs_fks
+  implicit none
+  private
+  public :: process_instance_t
+
+  type :: nlo_event_deps_t
+     type(phs_identifier_t), dimension(:), allocatable :: phs_identifiers
+  end type nlo_event_deps_t
+
+  type :: process_instance_t
+     type(phs_fks_t), pointer :: phs => null ()
+     type(nlo_event_deps_t) :: event_deps
+   contains
+     procedure :: init => process_instance_init
+     procedure :: setup_real_event_kinematics => pi_setup_real_event_kinematics
+  end type process_instance_t
+
+contains
+
+  subroutine process_instance_init (instance)
+    class(process_instance_t), intent(out), target :: instance
+    integer :: i
+    integer :: i_born, i_real
+    allocate (instance%phs)
+  end subroutine process_instance_init
+
+  subroutine pi_setup_real_event_kinematics (process_instance)
+    class(process_instance_t), intent(inout) :: process_instance
+    integer :: i_real, i
+    associate (event_deps => process_instance%event_deps)
+       i_real = 2
+       associate (phs => process_instance%phs)
+          allocate (phs%phs_identifiers (3))
+          call phs%phs_identifiers(1)%init ([1])
+          call phs%phs_identifiers(2)%init ([1,2])
+          call phs%phs_identifiers(3)%init ([1,2,3])
+          process_instance%event_deps%phs_identifiers = phs%phs_identifiers  ! Error: mismatch in array shapes.
+       end associate
+    end associate
+  end subroutine pi_setup_real_event_kinematics
+
+end module instances
+
+!!!!!
+
+program main
+  use instances, only: process_instance_t
+  implicit none
+  type(process_instance_t), allocatable, target :: process_instance
+  allocate (process_instance)
+  call process_instance%init ()
+  call process_instance%setup_real_event_kinematics ()
+  if (associated (process_instance%phs)) deallocate (process_instance%phs)
+  if (allocated (process_instance)) deallocate (process_instance)
+end program main
+! { dg-final { scan-tree-dump-times "__builtin_realloc" 2 "original" } }