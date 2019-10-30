12bc4345369eaa11f7dcd8a60d8b166fd034a573
Kito Cheng <kito.cheng@sifive.com>
2019-10-30 18:10:45 +0800

    Add full testcase for rvvsltint*

--- gcc/testsuite/gcc.target/riscv/rvv_compare-2.c
+++ gcc/testsuite/gcc.target/riscv/rvv_compare-2.c
@@ -8,16 +8,19 @@
 /* Takes the scalar type STYPE, vector class VCLASS (int or float), and
    the e and m value.  */
 #define VSLT(STYPE, VCLASS, EM, MLEN)                                          \
-  void vlt##VCLASS##EM(size_t n, STYPE *x, STYPE *y, STYPE z) {                \
-    rvv##VCLASS##EM##_t vx, vy;                                                \
+  void vlt##VCLASS##EM(size_t n, STYPE *x, STYPE *y, STYPE *z) {               \
+    rvv##VCLASS##EM##_t vx, vy, vz;                                            \
     rvvbool##MLEN##_t mask;                                                    \
     vx = rvvld##VCLASS##EM(x);                                                 \
     vy = rvvld##VCLASS##EM(y);                                                 \
+    vz = rvvld##VCLASS##EM(z);                                                 \
     mask = rvvslt##VCLASS##EM(vx, vy);                                         \
+    mask = rvvslt##VCLASS##EM##_mask(mask, mask, vx, vz);                      \
     vx = rvvadd##VCLASS##EM##_mask (mask, vy, vx, vy);                         \
     rvvst##VCLASS##EM(x, vx);                                                  \
   }
 
 RVV_INT_TEST(VSLT)
 
-/* { dg-final { scan-assembler-times "vmslt.vv" 16 } } */
+/* XXX: Check only half of vmslt.vv has masking operand.  */
+/* { dg-final { scan-assembler-times "vmslt.vv" 32 } } */
