01d4671332a2e67125b875bca8ad67a8a858639a
zakk <zakk.chen@sifive.com>
2019-11-05 01:17:06 -0800

    Add testcase for implicit fmacc and fmadd

---
+++ gcc/testsuite/gcc.target/riscv/rvv_macc.c
@@ -0,0 +1,37 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -march=rv32gcv -mabi=ilp32f" } */
+
+#include "rvv-common.h"
+#include <riscv_vector.h>
+#include <stddef.h>
+
+/* Takes the scalar type STYPE, vector class VCLASS (int or float), and
+   the e and m value.  */
+#define VMACC(STYPE, VCLASS, EM, MLEN)                                         \
+  void vmacc##VCLASS##EM(size_t n, STYPE *x, STYPE *y, STYPE *z) {             \
+    rvv##VCLASS##EM##_t vx, vy, vz;                                            \
+    vx = rvvld##VCLASS##EM(x);                                                 \
+    vy = rvvld##VCLASS##EM(y);                                                 \
+    vz = rvvld##VCLASS##EM(z);                                                 \
+    vz = vx * vy + vz;                                                         \
+    rvvst##VCLASS##EM(z, vz);                                                  \
+  }
+
+#define VMACC_IMM(STYPE, VCLASS, EM, MLEN)                                     \
+  void vmacc##VCLASS##EM##_imm(size_t n, STYPE x, STYPE *y, STYPE *z) {        \
+    rvv##VCLASS##EM##_t vx, vy, vz;                                            \
+    vy = rvvld##VCLASS##EM(y);                                                 \
+    vz = rvvld##VCLASS##EM(z);                                                 \
+    vz = x * vy + vz;                                                          \
+    rvvst##VCLASS##EM(z, vz);                                                  \
+  }
+
+#if 0
+RVV_INT_TEST(VMACC)
+#endif
+
+RVV_FLOAT_TEST(VMACC)
+RVV_FLOAT_TEST(VMACC_IMM)
+
+/* { dg-final { scan-assembler-times "vf(?:macc|madd)\.vv" 12 } } */
+/* { dg-final { scan-assembler-times "vf(?:macc|madd)\.vf" 12 } } */
