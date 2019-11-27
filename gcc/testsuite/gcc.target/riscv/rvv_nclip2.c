bf11b7e38c056c84cf6adf567088b36406bdd365
Kito Cheng <kito.cheng@sifive.com>
2019-11-27 17:24:50 +0800

    Implement all vector narrowing fixed-point clip instructions in inline asm

---
+++ gcc/testsuite/gcc.target/riscv/rvv_nclip2.c
@@ -0,0 +1,16 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -march=rv32gcv -mabi=ilp32f" } */
+
+#include <riscv_vector.h>
+#include <stddef.h>
+#include "rvv-common.h"
+
+RVV_WINT_TEST_ARG(RVV_NINT_BIN_BUILTIN_VEC_SCALAR_IMM_MASKED_TEST, nclip)
+RVV_WUINT_TEST_ARG(RVV_NINT_BIN_BUILTIN_VEC_SCALAR_IMM_MASKED_TEST, nclip)
+
+/* { dg-final { scan-assembler-times "vnclip.wv" 9 } } */
+/* { dg-final { scan-assembler-times "vnclip.wx" 9 } } */
+/* { dg-final { scan-assembler-times "vnclip.wi" 9 } } */
+/* { dg-final { scan-assembler-times "vnclipu.wv" 9 } } */
+/* { dg-final { scan-assembler-times "vnclipu.wx" 9 } } */
+/* { dg-final { scan-assembler-times "vnclipu.wi" 9 } } */
