8ba5f0d7370ad6763dcf22ec72daadfee2539d32
Monk Chiang <monk.chiang@sifive.com>
2020-04-15 12:03:38 +0800

    Update vector integer divide instructions testcases.

--- gcc/testsuite/gcc.target/riscv/rvv_div.c
+++ gcc/testsuite/gcc.target/riscv/rvv_div.c
@@ -7,20 +7,21 @@
 
 /* Takes the scalar type STYPE, vector class VCLASS (int or float), and
    the e and m value.  */
-#define VDIV(STYPE, VCLASST, VCLASS, EM, MLEN)                                          \
-  void vdiv##VCLASS##EM(size_t n, STYPE *x, STYPE *y, STYPE z) {               \
-    v##VCLASST##EM##_t vx, vy;                                                \
-    vx = vload_##VCLASS##EM(x);                                               \
-    vy = vload_##VCLASS##EM(y);                                               \
-    vy = vx / vy;                                                              \
-    vy = vy / z;                                                               \
-    vstore_##VCLASS##EM(y, vy);                                                \
+#define VDIV(STYPE, VCLASST, VCLASS, EM, MLEN)				\
+  void vdiv##VCLASS##EM(size_t n, STYPE *x, STYPE *y, STYPE z) {	\
+    v##VCLASST##EM##_t vx, vy;						\
+    vx = vload_##VCLASS##EM(x);						\
+    vy = vload_##VCLASS##EM(y);						\
+    vy = vx / vy;							\
+    vy = vy / z;							\
+    vstore_##VCLASS##EM(y, vy);						\
   }
 
-
-/* integer intrinsics have not finished */
-//RVV_INT_TEST(VDIV)
+RVV_INT_TEST(VDIV)
+RVV_UINT_TEST(VDIV)
 RVV_FLOAT_TEST(VDIV)
 
+/* { dg-final { scan-assembler-times "vdiv.vv" 32 } } */
+/* { dg-final { scan-assembler-times "vdivu.vv" 32 } } */
 /* { dg-final { scan-assembler-times "vfdiv.vv" 12 } } */
 /* { dg-final { scan-assembler-times "vfdiv.vf" 12 } } */
--- gcc/testsuite/gcc.target/riscv/rvv_div2.c
+++ gcc/testsuite/gcc.target/riscv/rvv_div2.c
@@ -5,23 +5,39 @@
 #include <stddef.h>
 #include "rvv-common.h"
 
-/* Takes the scalar type STYPE, vector class VCLASS (int or float), and
+/* Takes the scalar type STYPE, vector class VCLASS for int, and
    the e and m value.  */
-#define VDIV(STYPE, VCLASST, VCLASS, EM, MLEN)                                          \
-  void vmul##VCLASS##EM(size_t n, STYPE *x, STYPE *y, STYPE z) {               \
-    v##VCLASST##EM##_t vx, vy;                                                \
-    vx = vload_##VCLASS##EM(x);                                                 \
-    vy = vload_##VCLASS##EM(y);                                                 \
-    vy = vdiv_vv_##VCLASS##EM(vx, vy);                                      \
-    vy = vdiv_vs_##VCLASS##EM (vy, z);                                      \
-    vy = vrdiv_vs_##VCLASS##EM (vy, z);                                     \
-    vstore_##VCLASS##EM(y, vy);                                                  \
+#define INT_VDIV(STYPE, VCLASST, VCLASS, EM, MLEN)			\
+  void vdiv##VCLASS##EM(size_t n, STYPE *x, STYPE *y, STYPE z) {	\
+    v##VCLASST##EM##_t vx, vy;						\
+    vx = vload_##VCLASS##EM(x);						\
+    vy = vload_##VCLASS##EM(y);						\
+    vy = vdiv_vv_##VCLASS##EM(vx, vy); 					\
+    vy = vdiv_vs_##VCLASS##EM (vy, z); 					\
+    vstore_##VCLASS##EM(y, vy);						\
   }
 
-/* integer intrinsics have not finished */
-//RVV_INT_TEST(VDIV)
-RVV_FLOAT_TEST(VDIV)
+/* Takes the scalar type STYPE, vector class VCLASS for float, and
+   the e and m value.  */
+#define FLOAT_VDIV(STYPE, VCLASST, VCLASS, EM, MLEN)			\
+  void vfdiv##VCLASS##EM(size_t n, STYPE *x, STYPE *y, STYPE z) {	\
+    v##VCLASST##EM##_t vx, vy;						\
+    vx = vload_##VCLASS##EM(x);						\
+    vy = vload_##VCLASS##EM(y);						\
+    vy = vdiv_vv_##VCLASS##EM(vx, vy); 					\
+    vy = vdiv_vs_##VCLASS##EM (vy, z); 					\
+    vy = vrdiv_vs_##VCLASS##EM (vy, z);					\
+    vstore_##VCLASS##EM(y, vy);						\
+  }
 
+RVV_INT_TEST(INT_VDIV)
+RVV_UINT_TEST(INT_VDIV)
+RVV_FLOAT_TEST(FLOAT_VDIV)
+
+/* { dg-final { scan-assembler-times "vdiv.vv" 16 } } */
+/* { dg-final { scan-assembler-times "vdiv.vx" 16 } } */
+/* { dg-final { scan-assembler-times "vdivu.vv" 16 } } */
+/* { dg-final { scan-assembler-times "vdivu.vx" 16 } } */
 /* { dg-final { scan-assembler-times "vfdiv.vv" 12 } } */
 /* { dg-final { scan-assembler-times "vfdiv.vf" 12 } } */
 /* { dg-final { scan-assembler-times "vfrdiv.vf" 12 } } */
