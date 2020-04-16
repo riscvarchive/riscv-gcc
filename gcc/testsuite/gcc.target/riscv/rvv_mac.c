5b4572967eae0d71d0b483391cf2c68347c9bcb1
Monk Chiang <monk.chiang@sifive.com>
2020-04-16 16:05:23 +0800

    Update testcase for Vector Single-Width Integer Multiply-Add Instructions.

--- gcc/testsuite/gcc.target/riscv/rvv_mac.c
+++ gcc/testsuite/gcc.target/riscv/rvv_mac.c
@@ -9,7 +9,10 @@ RVV_INT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, macc)
 RVV_INT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, nmsac)
 RVV_INT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, madd)
 RVV_INT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, nmsub)
-
+RVV_UINT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, macc)
+RVV_UINT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, nmsac)
+RVV_UINT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, madd)
+RVV_UINT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, nmsub)
 RVV_FLOAT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, macc)
 RVV_FLOAT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, nmacc)
 RVV_FLOAT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, msac)
@@ -19,14 +22,10 @@ RVV_FLOAT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, nmadd)
 RVV_FLOAT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, msub)
 RVV_FLOAT_TEST_ARG(RVV_MAC_BUILTIN_VEC_SCALAR_MASKED_TEST, nmsub)
 
-/* { dg-final { scan-assembler-times "vmacc.vv" 32 } } */
-/* { dg-final { scan-assembler-times "vmacc.vx" 32 } } */
-/* { dg-final { scan-assembler-times "vnmsac.vv" 32 } } */
-/* { dg-final { scan-assembler-times "vnmsac.vx" 32 } } */
-/* { dg-final { scan-assembler-times "vmadd.vv" 32 } } */
-/* { dg-final { scan-assembler-times "vmadd.vx" 32 } } */
-/* { dg-final { scan-assembler-times "vnmsub.vv" 32 } } */
-/* { dg-final { scan-assembler-times "vnmsub.vx" 32 } } */
+/* { dg-final { scan-assembler-times "v(?:macc|madd)\.vv" 128 } } */
+/* { dg-final { scan-assembler-times "v(?:macc|madd)\.vx" 128 } } */
+/* { dg-final { scan-assembler-times "v(?:nmsac|nmsub)\.vv" 128 } } */
+/* { dg-final { scan-assembler-times "v(?:nmsac|nmsub)\.vx" 128 } } */
 
 /* { dg-final { scan-assembler-times "vfmacc.vv|vfmadd.vv" 48 } } */
 /* { dg-final { scan-assembler-times "vfmacc.vf" 24 } } */
