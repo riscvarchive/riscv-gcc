6cd5c7f4230a6dd6c38bfbb10133933fc11a88db
Monk Chiang <monk.chiang@sifive.com>
2020-05-07 10:26:03 +0800

    Update testcase for vcompress instruction.

--- gcc/testsuite/gcc.target/riscv/rvv_compress.c
+++ gcc/testsuite/gcc.target/riscv/rvv_compress.c
@@ -5,15 +5,15 @@
 #include <stddef.h>
 #include "rvv-common.h"
 
-#define RVV_TEST_COMPRESS(STYPE, VCLASST, VCLASS, EM, MLEN)		\
+#define RVV_TEST_COMPRESS(STYPE, VCLASST, VCLASS, EM, MLEN)	\
   void test_compress_##VCLASS##EM (STYPE *x, STYPE *y, long s) {\
     v##VCLASST##EM##_t vx;					\
     v##VCLASST##EM##_t vy;					\
     vbool##MLEN##_t mask;					\
-    vx = vload_##VCLASS##EM(x);				\
-    vy = vload_##VCLASS##EM(y);				\
-    mask = vset_b##MLEN ();				\
-    vx = vcompress_vm_##VCLASS##EM(vx, mask);		\
+    vx = vload_##VCLASS##EM(x);					\
+    vy = vload_##VCLASS##EM(y);					\
+    mask = vset_b##MLEN ();					\
+    vx = vcompress_vm_##VCLASS##EM##_mask(mask, vx, vy);	\
     vstore_##VCLASS##EM(x, vx);					\
   }								\
 
