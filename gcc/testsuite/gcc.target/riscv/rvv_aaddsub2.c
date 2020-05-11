2e0ae2983132ffe7f5252989d90ee9e47cf11a75
Monk Chiang <monk.chiang@sifive.com>
2020-05-11 14:16:10 +0800

    Update testcase for integer averaging add and subtract instructions.

--- gcc/testsuite/gcc.target/riscv/rvv_aaddsub2.c
+++ gcc/testsuite/gcc.target/riscv/rvv_aaddsub2.c
@@ -11,11 +11,9 @@ RVV_INT_TEST_ARG(RVV_BIN_BUILTIN_VEC_SCALAR_IMM_MASKED_TEST, asub)
 RVV_UINT_TEST_ARG(RVV_BIN_BUILTIN_VEC_SCALAR_IMM_MASKED_TEST, asub)
 
 /* { dg-final { scan-assembler-times "vaadd.vv" 16 } } */
-/* { dg-final { scan-assembler-times "vaadd.vx" 16 } } */
-/* { dg-final { scan-assembler-times "vaadd.vi" 16 } } */
+/* { dg-final { scan-assembler-times "vaadd.vx" 32 } } */
 /* { dg-final { scan-assembler-times "vaaddu.vv" 16 } } */
-/* { dg-final { scan-assembler-times "vaaddu.vx" 16 } } */
-/* { dg-final { scan-assembler-times "vaaddu.vi" 16 } } */
+/* { dg-final { scan-assembler-times "vaaddu.vx" 32 } } */
 /* { dg-final { scan-assembler-times "vasub.vv" 16 } } */
 /* { dg-final { scan-assembler-times "vasub.vx" 32 } } */
 /* { dg-final { scan-assembler-times "vasubu.vv" 16 } } */
