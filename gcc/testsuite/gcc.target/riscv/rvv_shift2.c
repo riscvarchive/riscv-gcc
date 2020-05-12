bc7d3787d50b4168cf25050c9be09bbf71a2492a
Monk Chiang <monk.chiang@sifive.com>
2020-05-12 16:39:05 +0800

    Update testcase for vssrl and vssra instructions.

--- gcc/testsuite/gcc.target/riscv/rvv_shift2.c
+++ gcc/testsuite/gcc.target/riscv/rvv_shift2.c
@@ -10,8 +10,6 @@ RVV_SEW_UINT_TEST_ARG(RVV_SHIFT_VEC_SCALAR_MASKED_TEST, sll)
 RVV_SEW_INT_TEST_ARG(RVV_SHIFT_VEC_SCALAR_MASKED_TEST, sra)
 RVV_SEW_UINT_TEST_ARG(RVV_SHIFT_VEC_SCALAR_MASKED_TEST, srl)
 RVV_SEW_INT_TEST_ARG(RVV_SHIFT_VEC_SCALAR_MASKED_TEST, ssra)
-RVV_SEW_UINT_TEST_ARG(RVV_SHIFT_VEC_SCALAR_MASKED_TEST, ssra)
-RVV_SEW_INT_TEST_ARG(RVV_SHIFT_VEC_SCALAR_MASKED_TEST, ssrl)
 RVV_SEW_UINT_TEST_ARG(RVV_SHIFT_VEC_SCALAR_MASKED_TEST, ssrl)
 
 /* { dg-final { scan-assembler-times "vsll.vv" 32 } } */
@@ -20,7 +18,7 @@ RVV_SEW_UINT_TEST_ARG(RVV_SHIFT_VEC_SCALAR_MASKED_TEST, ssrl)
 /* { dg-final { scan-assembler-times "vsra.vx" 16 } } */
 /* { dg-final { scan-assembler-times "vsrl.vv" 16 } } */
 /* { dg-final { scan-assembler-times "vsrl.vx" 16 } } */
-/* { dg-final { scan-assembler-times "vssra.vv" 32 } } */
-/* { dg-final { scan-assembler-times "vssra.vx" 32 } } */
-/* { dg-final { scan-assembler-times "vssrl.vv" 32 } } */
-/* { dg-final { scan-assembler-times "vssrl.vx" 32 } } */
+/* { dg-final { scan-assembler-times "vssra.vv" 16 } } */
+/* { dg-final { scan-assembler-times "vssra.vx" 16 } } */
+/* { dg-final { scan-assembler-times "vssrl.vv" 16 } } */
+/* { dg-final { scan-assembler-times "vssrl.vx" 16 } } */
