f4f71205cff18d5d877a9b7551647f3f0432558c
Monk Chiang <monk.chiang@sifive.com>
2020-07-10 13:16:46 +0800

    Split constant value, when immediate is over range in a ADD instruction.

--- gcc/config/riscv/riscv.c
+++ gcc/config/riscv/riscv.c
@@ -4295,7 +4295,17 @@ riscv_gen_load_poly_int (rtx target, rtx tmp1, rtx tmp2, poly_int64 value)
   if (scalar_offset)
     {
       emit_insn (insn);
-      return gen_add3_insn (target, target, GEN_INT (scalar_offset));
+
+      if (SMALL_OPERAND (scalar_offset))
+	return gen_add3_insn (target, target, GEN_INT (scalar_offset));
+      else
+	{
+	  HOST_WIDE_INT max_addi_value = IMM_REACH/2 - 1;
+	  emit_insn (gen_add3_insn (target, target,
+				    GEN_INT (max_addi_value)));
+	  return gen_add3_insn (target, target,
+				GEN_INT (scalar_offset - max_addi_value));
+	}
     }
   else
     return insn;
