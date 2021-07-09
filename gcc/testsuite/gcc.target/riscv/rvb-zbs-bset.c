73055647d33c0b63a3125c372019d1dac0f8ac34
jiawei <jiawei@iscas.ac.cn>
2021-07-09 19:46:59 +0800

    Update testcases with ZBS and reset version to 0.93

--- gcc/testsuite/gcc.target/riscv/rvb-zbs-bset.c
+++ gcc/testsuite/gcc.target/riscv/rvb-zbs-bset.c
@@ -1,61 +1,64 @@
 /* { dg-do compile } */
 /* { dg-options "-march=rv64gc_zbs -mabi=lp64 -O2" } */
 
-/* sbset */
+/* bset */
 long
 sub0 (long i, long j)
 {
   return i | (1L << j);
 }
 
-/* sbset_mask */
+/* bset_mask */
 long
 sub1 (long i, long j)
 {
   return i | (1L << (j & 0x3f));
 }
 
-/* sbset_1 */
+/* bset_1 */
 long
 sub2 (long i)
 {
   return 1L << i;
 }
 
-/* sbset_1_mask */
+/* bset_1_mask */
 long
 sub3 (long i)
 {
   return 1L << (i & 0x3f);
 }
 
-/* sbseti */
+/* bseti */
 long
 sub4 (long i)
 {
   return i | (1L << 20);
 }
 
-/* sbsetw */
+/* bsetw */
 int
 sub5 (int i, int j)
 {
   return i | (1 << j);
 }
 
-/* sbsetw_mask */
+/* bsetw_mask */
 int
 sub6 (int i, int j)
 {
   return i | (1 << (j & 0x1f));
 }
 
-/* sbsetiw */
+/* bsetiw */
 int
 sub7 (long i)
 {
   return (int)i | (1 << 20);
 }
 
-/* { dg-final { scan-assembler-times "bset" 8 } } */
+/* { dg-final { scan-assembler-times "bset" 9 } } */
+/* { dg-final { scan-assembler-times "bseti" 2 } } */
+/* { dg-final { scan-assembler-times "bsetw" 2 } } */
+/* { dg-final { scan-assembler-times "bsetiw" 1 } } */
 /* { dg-final { scan-assembler-not "andi" } } */
