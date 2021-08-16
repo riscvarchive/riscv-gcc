26c9cead860923d07f4c31a84627a0222a9bf1b6
Kito Cheng <kito.cheng@sifive.com>
2021-08-16 15:57:42 +0800

    Fix rvv-large-stack.C

--- gcc/testsuite/g++.target/riscv/rvv-large-stack.C
+++ gcc/testsuite/g++.target/riscv/rvv-large-stack.C
@@ -10,8 +10,8 @@ __attribute__((noinline)) void NestedFunc(vbool32_t a, vbool32_t b) {
 
 __attribute__((noinline)) void Crash() {
   std::mt19937 rng;
-  const vuint32m1_t v0 = vmv_v_x_u32m1(0);
-  const vbool32_t mask = vmseq_vv_u32m1_b32(v0, v0);
+  const vuint32m1_t v0 = vmv_v_x_u32m1(0, 10);
+  const vbool32_t mask = vmseq_vv_u32m1_b32(v0, v0, 10);
   NestedFunc(mask, mask);
 }
 
@@ -23,4 +23,4 @@ int main() {
 /* { dg-final { scan-assembler "mul\tt1,t1,t2" } } */
 /* { dg-final { scan-assembler "li\tt2,-4096" } } */
 /* { dg-final { scan-assembler "add\tt2,t2,t1" } } */
-/* { dg-final { scan-assembler "addi\tt1,t2,1552" } } */
+/* { dg-final { scan-assembler "addi\tt1,t2," } } */
